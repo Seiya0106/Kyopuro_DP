854765f78e789781f9f9eecd7cb867333752a8af
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2026-02-23 14:34:39 +0900

    Frog 2

---
+++ b.py
@@ -0,0 +1,9 @@
+n, k = map(int,input().split())
+h = list(map(int,input().split()))
+dp = [float("inf")] * n
+dp[0] = 0
+for i in range(n):
+    for j in range(1, k+1):
+        if i+j < n:
+            dp[i+j] = min(dp[i+j], dp[i] + abs(h[i+j] - h[i]))
+print(dp[n-1])

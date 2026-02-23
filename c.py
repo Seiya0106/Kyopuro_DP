b25dc45914bf58e21b715a300d6959b9dbe36dde
Seiya0106 <g2553458@cc.kyoto-su.ac.jp>
2026-02-23 20:20:05 +0900

    Vacation

---
+++ c.py
@@ -0,0 +1,14 @@
+n = int(input())
+a = [list(map(int,input().split())) for _ in range(n)]
+dp = [[0] * 3 for _ in range(n)]
+dp[0] = a[0]
+
+for i in range(1, n):
+    for j in range(3): # 今日の活動
+        for k in range(3): # 昨日の活動
+            if j != k:
+                dp[i][j] = max(dp[i][j], dp[i-1][k] + a[i][j])
+        print(dp)
+
+print(dp)
+print(max(dp[n-1]))
